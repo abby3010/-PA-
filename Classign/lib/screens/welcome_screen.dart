75345ed20788003c5b9a39ec97c4b9197d54076b
abby3010 <abhayubhale.30@gmail.com>
2020-05-23 17:37:30 +0530

    Created Classign files

--- Classign/lib/screens/welcome_screen.dart
+++ Classign/lib/screens/welcome_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import "constants.dart";
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
+    
     return MaterialApp(
       
+      theme: defaultTheme,
+      
+      home: Scaffold(
+       
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          
+          mainAxisAlignment:  MainAxisAlignment.center,
+
+          children: <Widget>[
+
+          Image( image: AssetImage('image/playstore.png'),),
+          
+
+        ],),
+      ),  
+      
+      
+      
+      ),
     );
   }
 }
--- README.md
+++
@@ -1,6 +0,0 @@
-# -PA-
-GitHub Repository for Inheritance Project
-Team Member 1: Abhay Ubhale
-Team Member 2: Purva Paresh Anjarlekar
-Team Member 3: Arvind Mundhe
-Team Member 4: Parvathy Nair
