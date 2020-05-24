9c77f5362fa5287f64af1e26620540343992a48d
abby3010 <abhayubhale.30@gmail.com>
2020-05-24 15:50:36 +0530

    Welcome Screen and Login Screen Uodate

--- Classign/lib/main.dart
+++ Classign/lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
+import 'screens/constants.dart';
+import 'screens/login_screen.dart';
 
 void main() {
-  runApp( WelcomeScreen() );
+  runApp( MaterialApp(
+    home: WelcomeScreen(),
+    debugShowCheckedModeBanner: false,
+    theme: defaultTheme,
+    initialRoute: WelcomeScreen.id,
+    routes: {
+      WelcomeScreen.id : (context) => WelcomeScreen(),
+      LoginScreen.id   : (context) => LoginScreen(),
+    },
+
+    ) 
+  );
+
+
+
 }
--- Classign/lib/screens/welcome_screen.dart
+++ Classign/lib/screens/welcome_screen.dart
@@ -1,34 +1,76 @@
 import 'package:flutter/material.dart';
-import "constants.dart";
+import 'login_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
+
+  static const String id = "welcome_screen";
+
   @override
   Widget build(BuildContext context) {
-    
-    
-    return MaterialApp(
-      
-      theme: defaultTheme,
-      
-      home: Scaffold(
-       
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          
-          mainAxisAlignment:  MainAxisAlignment.center,
+    return  Scaffold(
+        backgroundColor: Color(0xFFE6E2E1),
 
-          children: <Widget>[
+        body: Container(
+          decoration: BoxDecoration(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image(
+                    image: AssetImage('images/114.png'),
+                  ),
 
-          Image( image: AssetImage('image/playstore.png'),),
-          
+              Text("lAssign", style: TextStyle(color: Colors.black , fontSize: 50.0, fontFamily: 'SpecialElite' ), ),
 
-        ],),
-      ),  
-      
-      
-      
-      ),
-    );
+                ],
+              ),
+              
+
+              Image(
+                width: 600,
+                height: 250,
+                image: AssetImage('images/welcome.png'),
+              ),
+
+              SizedBox(height: 40.0),
+              
+            ButtonTheme(
+              minWidth: 150.0,
+              height: 50.0,
+              child: RaisedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, LoginScreen.id );
+                  },
+                  elevation: 10.0,
+                  child: Text("Login" , style:TextStyle(color: Colors.white , fontSize: 20.0) ,),
+                  color: Colors.blue[700],
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))
+                  ),
+              ),
+
+              SizedBox(height: 30.0),
+
+              ButtonTheme(
+              minWidth: 150.0,
+              height: 50.0,
+              child: RaisedButton(
+                  onPressed: (){
+                    // Navigator.pushNamed(context, SignUpScreen.id );
+                  },
+                  elevation: 10.0,
+                  child: Text("Sign Up" , style:TextStyle(color: Colors.white , fontSize: 20.0) ,),
+                  color: Colors.blue[700],
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))
+                  ),
+              ),
+              
+              // Text("Welcome" , style: ,),
+
+            ],
+          ),
+        ),
+      );
   }
 }
